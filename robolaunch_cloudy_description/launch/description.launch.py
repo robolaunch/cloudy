16b38cebebfccee13a16a4b9443bfb2355d904d5
mertgungor <gungor.mert@metu.edu.tr>
2023-01-24 10:30:41 +0200

    fix

--- robolaunch_cloudy_description/launch/description.launch.py
+++ robolaunch_cloudy_description/launch/description.launch.py
@@ -13,11 +13,15 @@ def generate_launch_description():
     
 
     cloudy_v1_urdf_path = PathJoinSubstitution(
-        [FindPackageShare('robolaunch_cloudy_description'), 'urdf', 'cloudy_v1.urdf.xacro']
+        [FindPackageShare('robolaunch_cloudy_description'), 'urdf', 'robots', 'cloudy_v1.urdf.xacro']
     ),
 
     cloudy_v2_urdf_path = PathJoinSubstitution(
-        [FindPackageShare('robolaunch_cloudy_description'), 'urdf', 'cloudy_v2.urdf.xacro']
+        [FindPackageShare('robolaunch_cloudy_description'), 'urdf', 'robots', 'cloudy_v2.urdf.xacro']
+    )
+
+    arcelik_urdf_path = PathJoinSubstitution(
+        [FindPackageShare('robolaunch_cloudy_description'), 'urdf', 'robots', 'arcelik', 'mecanum.urdf.xacro']
     )
 
     
@@ -32,6 +36,12 @@ def generate_launch_description():
         DeclareLaunchArgument(
             name='cloudy_v2_urdf', 
             default_value=cloudy_v2_urdf_path,
+            description='URDF path'
+        ),
+
+        DeclareLaunchArgument(
+            name='arcelik_urdf', 
+            default_value=arcelik_urdf_path,
             description='URDF path'
         ),
 
@@ -66,6 +76,17 @@ def generate_launch_description():
                 'use_sim_time': use_sim_time
             }],
             condition=IfCondition(PythonExpression([LaunchConfiguration("vehicle"), " == 'cloudy_v2'"]))
+        ),
+
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            output='screen',
+            parameters=[{
+                'robot_description': Command(['xacro ', LaunchConfiguration('arcelik_urdf')]), 
+                'use_sim_time': use_sim_time
+            }],
+            condition=IfCondition(PythonExpression([LaunchConfiguration("vehicle"), " == 'arcelik'"]))
         ),
 
         
