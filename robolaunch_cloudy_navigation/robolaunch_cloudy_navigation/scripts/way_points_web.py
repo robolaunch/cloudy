4d47697f379faea50b19ab949bb3818ff7a14dcf
mertgungor <gungor.mert@metu.edu.tr>
2023-03-16 13:47:57 +0200

    tf2 position publisher

--- robolaunch_cloudy_navigation/robolaunch_cloudy_navigation/scripts/way_points_web.py
+++ robolaunch_cloudy_navigation/robolaunch_cloudy_navigation/scripts/way_points_web.py
@@ -9,7 +9,8 @@ from rclpy.duration import Duration
 import time
 import copy
 import math
-
+import tf2_ros
+import os
 
 class WayPointsWeb(Node):
 
@@ -29,11 +30,40 @@ class WayPointsWeb(Node):
         initial_pose.pose.orientation.z = 0.0
         initial_pose.pose.orientation.w = 1.0
 
+        self.t = tf2_ros.TransformStamped()
+        self.pose = PoseStamped()
+
         self.navigator.setInitialPose(initial_pose)
 
         time.sleep(5)
 
         self.subscription = self.create_subscription(String, 'way_points_web', self.waypoints_callback, 10)
+        self.pose_publisher = self.create_publisher(PoseStamped, 'robot_position', 10)
+
+        # create tf2 buffer and listener
+        self.tf_buffer = tf2_ros.Buffer()
+        self.tf_listener = tf2_ros.TransformListener(self.tf_buffer, self)
+
+
+        self.create_timer(0.1, self.timer_callback)
+
+
+    def timer_callback(self):
+        
+        
+        self.t = self.tf_buffer.lookup_transform('odom', 'base_link', rclpy.time.Time().to_msg(), rclpy.time.Duration(seconds=1.0))
+        self.pose = PoseStamped()
+        self.pose.pose.position.x = self.t.transform.translation.x
+        self.pose.pose.position.y = self.t.transform.translation.y
+        self.pose.pose.position.z = self.t.transform.translation.z
+        self.pose.pose.orientation.x = self.t.transform.rotation.x
+        self.pose.pose.orientation.y = self.t.transform.rotation.y
+        self.pose.pose.orientation.z = self.t.transform.rotation.z
+        self.pose.pose.orientation.w = self.t.transform.rotation.w
+
+        self.pose_publisher.publish(self.pose)
+
+        # print(os.system("ros2 run tf2_ros tf2_echo map base_link").split("\n"))
 
 
     def waypoints_callback(self, msg):
